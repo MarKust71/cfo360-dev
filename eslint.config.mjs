89bbd251abea27f14b9d17752daeace5808aea7a
Marek Kustosz <markust71@hotmail.com>
2025-03-02 00:09:03 +0100

    add Prettier and ESLint configuration; update rules and create new files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
 import { FlatCompat } from "@eslint/eslintrc";
+import eslintPluginPrettier from "eslint-plugin-prettier";
 import eslintPluginSimpleImportSort from "eslint-plugin-simple-import-sort";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
@@ -15,11 +16,21 @@ const eslintConfig = [
   {
     plugins: {
       "simple-import-sort": eslintPluginSimpleImportSort,
+      prettier: eslintPluginPrettier,
     },
     rules: {
       "simple-import-sort/imports": "error",
       "simple-import-sort/exports": "error",
       "no-multiple-empty-lines": ["error", { max: 1, maxEOF: 1, maxBOF: 0 }],
+      "padding-line-between-statements": [
+        "error",
+        { blankLine: "always", prev: "*", next: "return" },
+        { blankLine: "never", prev: "block", next: "return" },
+        { blankLine: "always", prev: "*", next: "return" },
+        { blankLine: "never", prev: "block", next: "return" },
+      ],
+      "object-curly-spacing": ["error", "always"],
+      "prettier/prettier": "error",
     },
   },
 ];
---
+++ src/app/api/webhooks/mailerlite/route.ts
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  const {
+    payload: { events },
+  } = await req.json();
+
+  const subscriberAddedToGroup = events.find(
+    ({ type }: { type: string }) => type === "subscriber.added_to_group"
+  );
+
+  // const {
+  //   type,
+  //   subscriber: {
+  //     id: subscriberId,
+  //     email,
+  //     fields: { name },
+  //   },
+  //   group: { id: groupId },
+  // } = subscriberAddedToGroup;
+  //
+  // const result = await addSubscriber(
+  //   { email, mailerLiteId: subscriberId, name }
+  // )
+  //
+  // if (result.error) {
+  //   console.error({ error: result.error })
+  //
+  //   return new NextResponse(
+  //     JSON.stringify({ error: result.error }), { status: 400 }
+  //   )
+  // }
+
+  return new NextResponse(
+    // JSON.stringify({ type, subscriberId, email, groupId, name }),
+    JSON.stringify(subscriberAddedToGroup),
+    { status: 201 }
+  );
+}
