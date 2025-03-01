631c233b9d25a6a0e105347743a01a5774f178b9
Marek Kustosz <markust71@hotmail.com>
2025-03-02 00:12:40 +0100

    Staging (#3)
    * update project structure and configuration; add ESLint rules and IDE settings (#1)
    * add Prettier and ESLint configuration; update rules and create new files (#2)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import eslintPluginSimpleImportSort from "eslint-plugin-simple-import-sort";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +13,26 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: {
+      "simple-import-sort": eslintPluginSimpleImportSort,
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "no-multiple-empty-lines": ["error", { max: 1, maxEOF: 1, maxBOF: 0 }],
+      "padding-line-between-statements": [
+        "error",
+        { blankLine: "always", prev: "*", next: "return" },
+        { blankLine: "never", prev: "block", next: "return" },
+        { blankLine: "always", prev: "*", next: "return" },
+        { blankLine: "never", prev: "block", next: "return" },
+      ],
+      "object-curly-spacing": ["error", "always"],
+      "prettier/prettier": "error",
+    },
+  },
 ];
 
 export default eslintConfig;
