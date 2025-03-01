8ac9044e74826467ba4999bf351bb7d099155834
Marek Kustosz <markust71@hotmail.com>
2025-03-02 00:11:48 +0100

    add Prettier and ESLint configuration; update rules and create new files (#2)

---
+++ prettier.config.mjs
@@ -0,0 +1,7 @@
+export default {
+  bracketSpacing: true, // Spacja wewnątrz {}
+  semi: true, // Wymuszanie średników
+  singleQuote: false, // Podwójne cudzysłowy
+  tabWidth: 2, // Wcięcia 2 spacje
+  trailingComma: "es5", // Przecinki w obiektach/tablicach
+};
