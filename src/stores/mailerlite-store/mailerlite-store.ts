6c24076442ba8a552e014f177160ac8f9d01b210
Marek Kustosz <markust71@hotmail.com>
2025-03-02 02:43:03 +0100

    add Mailerlite integration; create store and components, update styles (#4)

---
+++ src/stores/mailerlite-store/mailerlite-store.ts
@@ -0,0 +1,10 @@
+import { create } from "zustand";
+
+import { MailerliteStoreState } from "@/stores/mailerlite-store/mailerlite-store.types";
+
+export const useMailerliteStore = create<MailerliteStoreState>((set) => ({
+  data: {},
+  updateData: (newData) => {
+    set({ data: newData });
+  },
+}));
