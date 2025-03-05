52f3af26a59939d614d7508a4ce49e222d3f958d
Marek Kustosz <markust71@hotmail.com>
2025-03-05 18:45:34 +0100

    feat: update Mailerlite store state to use MailerliteSubscriber type for data handling

--- src/stores/mailerlite-store/mailerlite-store.types.ts
+++ src/stores/mailerlite-store/mailerlite-store.types.ts
@@ -3,6 +3,6 @@ import { MailerliteSubscriber } from "@/hooks/use-mailerlite.types";
 export type Data = Record<string, unknown>;
 
 export type MailerliteStoreState = {
-  data: Data;
+  data: MailerliteSubscriber;
   updateData: (newData: MailerliteSubscriber) => void;
 };
