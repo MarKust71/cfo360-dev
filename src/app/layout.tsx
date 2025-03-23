a40ab4039f893b38cc23bb0e2186f63a6de93016
Marek Kustosz <markust71@hotmail.com>
2025-03-23 21:19:36 +0100

    feat: clean up layout.tsx by removing unused font imports and comments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,11 @@
 import "./globals.css";
 
 import { UserProvider } from "@auth0/nextjs-auth0/client";
-// import { Geist, Geist_Mono } from "next/font/google";
 import { GeistMono } from "geist/font/mono";
 import { GeistSans } from "geist/font/sans";
 import type { Metadata } from "next";
 
 import { ThemeProvider } from "@/components/theme-provider";
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
 
 export const metadata: Metadata = {
   title: "CFO360",
@@ -32,7 +21,6 @@ export default function RootLayout({
     <html lang="pl">
       <UserProvider>
         <body
-          // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
           className={`${GeistSans.className} ${GeistMono.className} antialiased`}
         >
           <ThemeProvider
