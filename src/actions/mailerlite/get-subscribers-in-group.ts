53f7de0e8b77edf1df42e9c1c30c79abbc7e7a58
Marek Kustosz <markust71@hotmail.com>
2025-03-24 00:28:00 +0100

    feat: add Mailerlite group and subscriber retrieval functions with error handling

--- src/actions/mailerlite/get-subscribers-in-group.ts
+++ src/actions/mailerlite/get-subscribers-in-group.ts
@@ -1,6 +1,4 @@
-"use server";
-
-import MailerLite, { GetGroupsParams } from "@mailerlite/mailerlite-nodejs";
+import MailerLite, { SubscriberParams } from "@mailerlite/mailerlite-nodejs";
 
 import { CustomError } from "@/actions/mailerlite/get-groups.types";
 
@@ -8,10 +6,13 @@ const mailerlite = new MailerLite({
   api_key: process.env.MAILERLITE_API_KEY || "",
 });
 
-export const getGroups = async (params: GetGroupsParams) => {
+export const getSubscribersInGroup = async (
+  groupId: string,
+  params: SubscriberParams
+) => {
   try {
-    const response = await mailerlite.groups.get(params);
-    console.log(response.data);
+    const response = await mailerlite.groups.getSubscribers(groupId, params);
+    console.log(response.data.data[0].fields);
   } catch (error) {
     if (error instanceof Error && (error as CustomError).response) {
       console.error((error as CustomError).response?.data);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { getGroups } from "@/actions/mailerlite/get-groups";
+import { getSubscribersInGroup } from "@/actions/mailerlite/get-subscribers-in-group";
 import { MailerliteStore } from "@/components/mailerlite-store";
 import { Button } from "@/components/ui/button";
 
@@ -5,6 +7,19 @@ export const revalidate = 1;
 export const dynamicParams = true;
 
 export default async function Home() {
+  const handleClick = async () => {
+    "use server";
+
+    await getGroups({
+      filter: {
+        name: "cfo",
+      },
+      sort: "-created_at",
+    });
+
+    await getSubscribersInGroup("147700792230414307", {});
+  };
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -27,6 +42,8 @@ export default async function Home() {
         <a href="/api/auth/login">
           <Button>Log in</Button>
         </a>
+
+        <Button onClick={handleClick}>Get groups</Button>
       </main>
 
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
